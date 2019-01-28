3f2d64800da89f57e81492fcf41b1ab294424918
leahknafo <leahknafo1@gmail.com>
2019-01-28 15:02:09 +0200

    little fix

--- home.js
+++ home.js
@@ -131,7 +131,7 @@ function searchBySymbol() {
       }
     }
     buildBySearch();
-   
+
   })
 }
 
@@ -144,120 +144,122 @@ function buildBySearch() {
     t = t.replace(/{{id}}/g, d.id);
     $('#content').html(t);
 
-  
-});
 
-  }
+  });
+  collapseEvent();
+
+}
 
 
 //This array contains all the "id" of the currencies
 var idArray = [];
-  $.ajax('https://api.coingecko.com/api/v3/coins/list').done(function (d) {
-    for (i = 100; i < 200; i++) {
-      idArray.push(d[i].id);
-    }
-  });
-
-
-  //If the user wants to add a sixth currency to the list of reports, the function that deals with the modal will be called
-  var currenciesReport;
-  var tempArray;
-  var theSixth
-  function toggleChanges() {
-    let counter = 0;
-    let valid = false;
-    tempArray = [];
-
-    for (let i = 0; i < idArray.length; i++) {
-      if ($("#my" + idArray[i]).prop("checked") == true) {
-        counter++
-        tempArray.push(idArray[i]);
-      }
-    }
-    if (counter == 5) {
-      valid = true;
-      currenciesReport = tempArray;
-    }
-    if (counter > 5) {
-      // if(valid==false){//An extreme case in which a user attempts to add a sixth currency without having passed the fifth addition step
-      //   currenciesReport = tempArray; 
-      // }
-      findTheSixth();
-      appendModalTemplate();
-    }
-
-    console.log(currenciesReport);
+$.ajax('https://api.coingecko.com/api/v3/coins/list').done(function (d) {
+  for (i = 100; i < 200; i++) {
+    idArray.push(d[i].id);
   }
+});
 
 
-  //After comparing the array with five coins marked and the array with 6 coins marked. We find the last currency that has been marked. 
-  function findTheSixth() {
-    for (let i = 0; i < tempArray.length; i++) {
-      let temp = false;
-      for (let j = 0; j < currenciesReport.length; j++) {
-        if (currenciesReport[j] == tempArray[i]) {
-          temp = true;
-        }
-      }
-      if (temp == false) {
-        theSixth = tempArray[i];
-      }
+//If the user wants to add a sixth currency to the list of reports, the function that deals with the modal will be called
+var currenciesReport;
+var tempArray;
+var theSixth
+function toggleChanges() {
+  let counter = 0;
+  let valid = false;
+  tempArray = [];
+
+  for (let i = 0; i < idArray.length; i++) {
+    if ($("#my" + idArray[i]).prop("checked") == true) {
+      counter++
+      tempArray.push(idArray[i]);
     }
   }
+  if (counter == 5) {
+    valid = true;
+    currenciesReport = tempArray;
+  }
+  if (counter > 5) {
+    findTheSixth();
+    appendModalTemplate();
+  }
+}
+
+
+//After comparing the array with five coins marked and the array with 6 coins marked. We find the last currency that has been marked. 
+function findTheSixth() {
+  for (let i = 0; i < tempArray.length; i++) {
+    let temp = false;
+    for (let j = 0; j < currenciesReport.length; j++) {
+      if (currenciesReport[j] == tempArray[i]) {
+        temp = true;
+      }
+    }
+    if (temp == false) {
+      theSixth = tempArray[i];
+    }
+  }
+}
 
 
 
-  //The values ​​in the modal change accordingly and the modal is displayed
-  function appendModalTemplate() {
-    let t = modalTemplate;
-    let r = currenciesReport;
-    t = t.replace(/{{first}}/g, r[0]);
-    t = t.replace(/{{second}}/g, r[1]);
-    t = t.replace(/{{third}}/g, r[2]);
-    t = t.replace(/{{forth}}/g, r[3]);
-    t = t.replace(/{{fifth}}/g, r[4]);
-    $('#modal').html(t);
-    $("#myModal").modal();
-    cancelSwitching()
+//The values ​​in the modal change accordingly and the modal is displayed
+function appendModalTemplate() {
+  let t = modalTemplate;
+  let r = currenciesReport;
+  t = t.replace(/{{first}}/g, r[0]);
+  t = t.replace(/{{second}}/g, r[1]);
+  t = t.replace(/{{third}}/g, r[2]);
+  t = t.replace(/{{forth}}/g, r[3]);
+  t = t.replace(/{{fifth}}/g, r[4]);
+  $('#modal').html(t);
+  $("#myModal").modal();
+  cancelSwitching()
+}
+
+//The function checks whether a currency has been removed from the list. And if this is the case, the sixth coin marked on the spot will be inserted
+function modalToggleChange() {
+  console.log(currenciesReport)
+  for (let i = 0; i < currenciesReport.length; i++) {
+    if ($("#modal" + currenciesReport[i]).prop("checked") == false) { var removeItem = currenciesReport[i];
+      $("#savebutton").click(function () {
+        $("#my" + currenciesReport[i]).prop("checked", false);
+        $("#my" + theSixth).prop("checked", true);
+        currenciesReport = jQuery.grep(currenciesReport, function (value) {
+          return value != removeItem;
+        });
+        currenciesReport.push(theSixth);
+      })
+    }
   }
 
-  //The function checks whether a currency has been removed from the list. And if this is the case, the sixth coin marked on the spot will be inserted
-  function modalToggleChange() {
+}
+
+
+
+//By pressing the Cancel button or the Save button without changing. The sixth currency will not be placed in the list of reports
+function cancelSwitching() {
+  $(".close").click(function () {
+    let flag = true;
     for (let i = 0; i < currenciesReport.length; i++) {
-      if ($("#modal" + currenciesReport[i]).prop("checked") == false) {
-        $("#savebutton").click(function () {
-          $("#my" + currenciesReport[i]).prop("checked", false);
-          $("#my" + theSixth).prop("checked", true);
-        })
+      if ($("#my" + currenciesReport[i]).prop("checked") == false) {
+        flag = false;
       }
     }
-  }
-
-
-
-  //By pressing the Cancel button or the Save button without changing. The sixth currency will not be placed in the list of reports
-  function cancelSwitching() {
-    $(".close").click(function () {
-      let flag = true;
-      for (let i = 0; i < currenciesReport.length; i++) {
-        if ($("#my" + currenciesReport[i]).prop("checked") == false) {
-          flag = false;
-        }
+    if (flag == true) {
+      $("#my" + theSixth).prop("checked", false);
+    }
+  })
+  $("#savebutton").click(function () {
+    let flag = true;
+    for (let i = 0; i < currenciesReport.length; i++) {
+      if ($("#my" + currenciesReport[i]).prop("checked") == false) {
+        flag = false;
       }
-      if (flag == true) {
-        $("#my" + theSixth).prop("checked", false);
-      }
-    })
-    $("#savebutton").click(function () {
-      let flag = true;
-      for (let i = 0; i < currenciesReport.length; i++) {
-        if ($("#my" + currenciesReport[i]).prop("checked") == false) {
-          flag = false;
-        }
-      }
-      if (flag == true) {
-        $("#my" + theSixth).prop("checked", false);
-      }
-    })
+    }
+    if (flag == true) {
+      $("#my" + theSixth).prop("checked", false);
+    }
+  })
 
-  }
+}
